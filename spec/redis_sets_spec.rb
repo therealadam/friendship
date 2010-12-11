7f869e604ee539c4fb29e07d0a4755885492a143
Adam Keys <adam@therealadam.com>
2010-12-11 16:40:00 -0600

    Extract storage spec before adding another storage module.

--- spec/redis_sets_spec.rb
+++ spec/redis_sets_spec.rb
@@ -9,36 +9,7 @@ describe Friendship::Storage::RedisSets do
     Friendship::Storage::RedisSets.connection = redis
   end
 
-  it "creates and fetches relationships" do
-    subject.friend("john", "paul")
-    subject.friend("john", "george")
-    subject.friend("john", "ringo")
-
-    subject.friendships("john").should == %w{ringo george paul}
-  end
-
-  it "checks if one user is friends with another" do
-    subject.friend("john", "paul")
-    subject.friend("paul", "john")
-    
-    subject.friend_of?("john", "paul").should be_true
-  end
-
-  it "unfriends a user" do
-    subject.friend("john", "paul")
-    subject.unfriend("john", "paul")
-
-    subject.friend_of?("john", "paul").should be_false
-  end
-
-  it "finds mutual friends" do
-    subject.friend("john", "george")
-    subject.friend("john", "ringo")
-    subject.friend("paul", "george")
-    subject.friend("paul", "ringo")
-
-    subject.mutual_friends("john", "paul").should == ["ringo", "george"]
-  end
+  it_behaves_like "friendship storage"
 
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,37 @@
 require "friendship"
+
+shared_examples_for "friendship storage" do
+
+  it "creates and fetches relationships" do
+    subject.friend("john", "paul")
+    subject.friend("john", "george")
+    subject.friend("john", "ringo")
+
+    subject.friendships("john").should == %w{ringo george paul}
+  end
+
+  it "checks if one user is friends with another" do
+    subject.friend("john", "paul")
+    subject.friend("paul", "john")
+    
+    subject.friend_of?("john", "paul").should be_true
+  end
+
+  it "unfriends a user" do
+    subject.friend("john", "paul")
+    subject.unfriend("john", "paul")
+
+    subject.friend_of?("john", "paul").should be_false
+  end
+
+  it "finds mutual friends" do
+    subject.friend("john", "george")
+    subject.friend("john", "ringo")
+    subject.friend("paul", "george")
+    subject.friend("paul", "ringo")
+
+    subject.mutual_friends("john", "paul").should == ["ringo", "george"]
+  end
+
+end
+
